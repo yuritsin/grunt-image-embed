efa85a013d2b48a02b7152383f977237eaaf739c
Maksim Yuritsin <yuritsin.maksim@gmail.com>
2016-02-11 21:20:18 +0300

    Saving changes to source files

--- tasks/grunt-image-embed.js
+++ tasks/grunt-image-embed.js
@@ -8,46 +8,51 @@
 'use strict';
 
 module.exports = function(grunt) {
-  var encode = require('./lib/encode');
+	var encode = require('./lib/encode');
+	var fs = require('fs');
 
-  grunt.registerMultiTask('imageEmbed', 'Embed images as base64 data URIs inside your stylesheets', function() {
-    var opts = this.options();
-    var done = this.async();
-    var filesCount = this.files.length;
-    var doneCount = 0;
+	grunt.registerMultiTask('imageEmbed', 'Embed images as base64 data URIs inside your stylesheets', function() {
+		var opts = this.options();
+		var done = this.async();
+		var filesCount = this.files.length;
+		var doneCount = 0;
 
-    if (filesCount === 0) {
-      grunt.log.warn('No files defined');
-      return done();
-    }
+		if (filesCount === 0) {
+			grunt.log.warn('No files defined');
+			return done();
+		}
 
-    // Process each src file
-    this.files.forEach(function(file) {
-      var dest = file.dest;
-      var tasks;
+		// Process each src file
+		this.files.forEach(function(file) {
+			var dest = file.dest;
+			var tasks;
 
-      tasks = file.src.map(function(srcFile) {
-        return function(callback) {
-          encode.stylesheet(srcFile, opts, callback);
-        };
-      });
+			tasks = file.src.map(function (src) {
+				return new Promise(function(resolve, reject) {
+					if (src) {
+						encode.stylesheet(src, opts, function (err, output) {
+							fs.writeFile(src, output, function (err) {
+								if (err) {
+									grunt.log.writeln(err);
+									reject();
+								} else {
+									grunt.log.writeln(src);
+									resolve();
+								}
+							});
+						});
+					} else {
+						reject();
+					}
+				});
+			});
 
-      if(!tasks.length) {
-        grunt.log.writeln('No files found');
-        return done();
-      }
+			Promise.all(tasks).then(function (data) {
+				done();
+			});
 
-      // Once all files have been processed write them out.
-      grunt.util.async.parallel(tasks, function(err, output) {
-        grunt.file.write(dest, output);
-        grunt.log.writeln('File "' + dest + '" created.');
 
-        // call done() exactly once, after all files are processed
-        if(++doneCount >= filesCount) {
-          done();
-        }
-      });
-    });
-  });
+		});
+	});
 
 };
